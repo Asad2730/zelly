11685395149cef77d30ec917fa194c880b730e23
Asad2730 <110926054+Asad2730@users.noreply.github.com>
2023-02-21 00:02:51 +0500

    3rd

--- src/pages/user/ProductDetail.jsx
+++ src/pages/user/ProductDetail.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TitleCard from '../../components/Cards/user/TitleCard';
 import MainHeader from '../../components/header/MainHeader';
 import Footer from '../../sections/user/Footer';
-
+import axios from 'axios';
 
 export default function ProductDetail() {
 
 
     const id = localStorage.getItem('p_id')
-    console.log('id', id)
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        let url = `http://localhost:2000/api/product/getProductwithPk/${id}`;
+        axios.get(url)
+            .then(r => {
+                setData(r.data.data)
+            })
+            .catch(e => console.log(e))
+
+    }, [])
+
     return (
         <>
             <MainHeader />
@@ -16,31 +27,17 @@ export default function ProductDetail() {
             <section class="text-gray-400  body-font overflow-hidden">
                 <div class="container px-5 py-24 mx-auto">
                     <div class="lg:w-4/5 mx-auto flex flex-wrap">
-                        <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://flowbite.com/docs/images/products/apple-watch.png" />
+                        <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+                            src={`http://localhost:2000/uploads/${data['images']}`} />
                         <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                            <h2 class="text-sm title-font text-gray-500 tracking-widest">SHOP NAME</h2>
-                            <h1 class="text-black text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
-                            <div class="flex mb-4">
-                                <span class="flex items-center">
-                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-400" viewBox="0 0 24 24">
-                                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                                    </svg>
-                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-400" viewBox="0 0 24 24">
-                                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                                    </svg>
-                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-400" viewBox="0 0 24 24">
-                                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                                    </svg>
-                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-400" viewBox="0 0 24 24">
-                                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                                    </svg>
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-400" viewBox="0 0 24 24">
-                                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                                    </svg>
-                                    <span class="ml-3">4 Reviews</span>
-                                </span>
-                            </div>
-                            <p class="leading-relaxed text-black">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
+                            <h2 class="text-sm title-font text-gray-500 tracking-widest">{data['brandName']}</h2>
+                            <h1 class="text-black text-3xl title-font font-medium mb-1">{data['title']}</h1>
+
+                            <h2 class="text-sm title-font text-gray-500 tracking-widest">
+                                Available:{
+                                    data['available'] ? 'Yes' : 'No'
+                                }
+                            </h2>
                             <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-800 mb-5">
                                 <div class="flex text-black">
                                     <span class="mr-3">Color</span>
